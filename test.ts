ce8ef75ffab616a8ebdefd4691b2caf7badcb22c
eliassjogreen <eliassjogreen1@gmail.com>
2020-06-07 14:26:02 +0200

    chore: :arrow_up: Update test dependencies

--- test.ts
+++ test.ts
@@ -4,7 +4,7 @@ import { compress, decompress } from "./mod.ts";
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.54.0/testing/asserts.ts";
+} from "https://deno.land/std@0.56.0/testing/asserts.ts";
 
 function encode(input: string): Uint8Array {
   return new TextEncoder().encode(input);
